73a37fcf0bdc62d4aa5b3de45785b54646151def
DESKTOP-P6BU8DQ\hp <sonal.patel@archesoftronix.com>
2022-02-22 16:02:48 +0530

    Commit Changes of Personal info

---
+++ DemoCRUD/Controllers/PersonalInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoCRUD.Controllers
+{
+    public class PersonalInfoController : Controller
+    {
+        public ActionResult Index()
+        {
+            WorldEntities db = new WorldEntities();
+            return View(db.Country.ToList());
+        }
+    }
+}
